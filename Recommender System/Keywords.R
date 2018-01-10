becec3007f46f121485f2a7b2ccf7d5479c55826
DESKTOP-13AG486\Christian <christian.wnr@gmail.com>
2018-01-10 12:52:41 +0100

    Data generator

--- Recommender System/Keywords.R
+++ Recommender System/Keywords.R
@@ -1,5 +1,21 @@
-#Keywords for songs
+install.packages("RPostgreSQL")
+require("RPostgreSQL")
 
+connectPostgres <-function(){
+  pw <- {"pw"}
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv, dbname = "4PlayDB",
+                   host = "localhost", port = 5432,
+                   user = "postgres", password = pw)
+  rm(pw)
+  return (con)
+}
+
+con = connectPostgres()
+dbExistsTable(con, "album_content")
+dbGetQuery(con, "Insert into 'keyword' values (1,'hi')")
+
+#Keywords for songs
 keywords = c("love", "hate", "betrayal", "friendship", "relationship", "breakup",
              "drums", "bass", "piano", "guitar", "horn", "saxophone", "electronic guitar",
              "speed")
