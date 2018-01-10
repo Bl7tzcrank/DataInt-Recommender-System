d7c58e04b91fdce5ab0f6630e82bc533b0c2baca
dennismeis <dmeis@gmx.de>
2018-01-10 11:49:00 +0100

    added some keywords

--- Recommender System/Keywords.R
+++ Recommender System/Keywords.R
@@ -1,1 +1,5 @@
 #Keywords for songs
+
+keywords = c("love", "hate", "betrayal", "friendship", "relationship", "breakup",
+             "drums", "bass", "piano", "guitar", "horn", "saxophone", "electronic guitar",
+             "speed")
